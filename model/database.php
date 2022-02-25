75c1fdba65f70e8cbd050e4cc2433ffa10ecec2c
nczimmerman00 <n.zman@eaglecom.net>
2022-02-24 22:39:50 -0600

    second-commit

--- model/database.php
+++ model/database.php
@@ -1,13 +1,13 @@
 <?php 
-//$dsn = 'mysql:host=localhost;dbname=todolist';
-//$username = 'mgs_user';
-//$password = 'pa55word';
-$dsn = "mysql:host=localhost; port=3307; dbname=todolist";
-$username = 'root';
+$dsn = 'mysql:host=localhost;dbname=todolist';
+$username = 'mgs_user';
+$password = 'pa55word';
+//$dsn = "mysql:host=localhost; port=3307; dbname=todolist";
+//$username = 'root';
 
 try{
-    $db = new PDO($dsn, $username);
-    //$db = new PDO($dsn, $username, $password);
+    //$db = new PDO($dsn, $username);
+    $db = new PDO($dsn, $username, $password);
 }catch (PDOException $e){
     $error = 'Database Error: ';
     $error .= $e->getMessage();
